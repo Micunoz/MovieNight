8bd0d7ffa3fdce27036bf4f3ef85f3b69f2b3750
Micunoz <mmunoz@mines.edu>
2024-05-02 22:55:43 -0600

    user post and get

--- backend/src/users.js
+++ backend/src/users.js
@@ -5,7 +5,10 @@ const usersRouter = Router();
 
 //GET /user
 usersRouter.get("/", async (req, res) => {
+    const db = req.app.get("db");
+    const users = await db.collection("users").find().toArray();
 
+    return res.json(users);
 });
 
 //GET /user/:userId/favorite
@@ -20,7 +23,12 @@ usersRouter.get("/:userId/reviews", async (req, res) => {
 
 //POST /user
 usersRouter.post("/", async (req, res) => {
+    const db = req.app.get("db");
 
+    // body should have first name, last name, maybe fav movie, etc.
+    const result = await db.collection("users").insertOne(req.body);
+    console.info(result);
+    res.status(201).json(result.insertedId);
 });
 
 export default usersRouter;
